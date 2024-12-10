9144955f1509bab18a4d39e3fdfeeac52f7843e6
matecom57 <matecom57@gmail.com>
2024-12-10 05:36:29 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Don Clusterio'
+copyright = '2024, Santosg'
+author = 'Santosg'
 
 release = '0.1'
 version = '0.1.0'
